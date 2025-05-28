35aedc5a7ea34a3c7226ed5b230a2ec83b8705e4
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-05-28 14:57:51 +0200

    Main screens

--- app/src/main/java/com/francotte/myrecipesstore/MainActivity.kt
+++ app/src/main/java/com/francotte/myrecipesstore/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.francotte.myrecipesstore.ui.compose.MealSearchScreen
 import com.francotte.myrecipesstore.ui.theme.MyRecipesStoreTheme
+import com.francotte.myrecipesstore.ui.viewmodel.RecipeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +28,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyRecipesStoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        Spacer(Modifier.height(innerPadding.calculateTopPadding()))
+                        MealSearchScreen(hiltViewModel<RecipeViewModel>())
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyRecipesStoreTheme {
-        Greeting("Android")
     }
 }
