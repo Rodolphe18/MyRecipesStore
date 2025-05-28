5bc261c92f6ab95ed23d18d451fd8085a76bbc4f
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-05-28 14:57:35 +0200

    Composable to display list of meals

---
+++ app/src/main/java/com/francotte/myrecipesstore/ui/compose/MealGrid.kt
@@ -0,0 +1,34 @@
+package com.francotte.myrecipesstore.ui.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.francotte.myrecipesstore.model.AbstractMeal
+
+@Composable
+fun MealGrid(
+    meals: List<AbstractMeal>,
+    onFavoriteClick: (AbstractMeal) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(meals) { meal ->
+            MealItem(meal = meal, onFavoriteClick = onFavoriteClick)
+        }
+    }
+}
