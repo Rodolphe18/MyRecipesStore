62271155416294b90abca366dd1db3770246e98b
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-05-30 19:27:10 +0200

    Custom TopBAr & BottomBar composables created

---
+++ app/src/main/java/com/francotte/myrecipesstore/ui/navigation/BottomBar.kt
@@ -0,0 +1,127 @@
+package com.francotte.myrecipesstore.ui.navigation
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import com.francotte.myrecipesstore.R
+import com.francotte.myrecipesstore.ui.compose.home.HomeNavigationRoute
+import kotlin.reflect.KClass
+
+@Composable
+fun RowScope.NavigationBarItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    selectedIcon: @Composable () -> Unit = icon,
+    enabled: Boolean = true,
+    label: @Composable (() -> Unit)? = null,
+    alwaysShowLabel: Boolean = true
+) {
+    NavigationBarItem(
+        selected = selected,
+        onClick = onClick,
+        icon = if (selected) selectedIcon else icon,
+        modifier = modifier,
+        enabled = enabled,
+        label = label,
+        alwaysShowLabel = alwaysShowLabel,
+    )
+}
+
+@Composable
+fun NavigationBar(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    NavigationBar(
+        modifier = modifier,
+        tonalElevation = 0.dp,
+        content = content
+    )
+}
+
+@Composable
+fun BottomBar(
+    destinations: List<TopLevelDestination>,
+    onNavigateToDestination: (TopLevelDestination) -> Unit,
+    currentDestination: NavDestination?,
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier
+    ) {
+        destinations.forEach { destination ->
+            val selected = currentDestination.isTopLevelDestinationInHierarchy(destination)
+            NavigationBarItem(
+                selected = selected,
+                onClick = { onNavigateToDestination(destination) },
+                icon = {
+                    val icon = if (selected) {
+                        destination.selectedIcon
+                    } else {
+                        destination.unselectedIcon
+                    }
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(
+                        stringResource(destination.titleTextId),
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+            )
+        }
+    }
+}
+
+enum class TopLevelDestination(
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val titleTextId: Int,
+    val route: KClass<*>
+) {
+    HOME(
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home,
+        titleTextId = R.string.home,
+        route = HomeNavigationRoute::class
+    ),
+    CATEGORIES(
+        selectedIcon = Icons.Filled.ShoppingCart,
+        unselectedIcon = Icons.Outlined.ShoppingCart,
+        titleTextId = R.string.categories,
+        route = HomeNavigationRoute::class
+    ),
+    FAVORITES(
+        selectedIcon = Icons.Filled.Favorite,
+        unselectedIcon = Icons.Outlined.Favorite,
+        titleTextId = R.string.favorites,
+        route = HomeNavigationRoute::class
+    )
+}
+
+private fun NavDestination?.isTopLevelDestinationInHierarchy(destination: TopLevelDestination) =
+    this?.hierarchy?.any {
+        it.route?.contains(destination.name, true) ?: false
+    } ?: false
