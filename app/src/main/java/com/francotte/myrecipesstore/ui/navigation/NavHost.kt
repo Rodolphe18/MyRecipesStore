84eacdd52b5b15ad3e31e27c59ac17379ba0d5b7
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-05-30 19:40:00 +0200

    NavHost created with 3 tabs (home, categories, favorites)

---
+++ app/src/main/java/com/francotte/myrecipesstore/ui/navigation/NavHost.kt
@@ -0,0 +1,57 @@
+package com.francotte.myrecipesstore.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.francotte.myrecipesstore.ui.compose.categories.categoriesScreen
+import com.francotte.myrecipesstore.ui.compose.categories.category.categoryScreen
+import com.francotte.myrecipesstore.ui.compose.categories.category.navigateToCategoryScreen
+import com.francotte.myrecipesstore.ui.compose.detail.detailRecipeScreen
+import com.francotte.myrecipesstore.ui.compose.detail.navigateToDetailRecipeScreen
+import com.francotte.myrecipesstore.ui.compose.favorites.favoritesScreen
+import com.francotte.myrecipesstore.ui.compose.home.BaseRoute
+import com.francotte.myrecipesstore.ui.compose.home.homeScreen
+import com.francotte.myrecipesstore.ui.compose.section.navigateToSection
+import com.francotte.myrecipesstore.ui.compose.section.sectionScreen
+
+@Composable
+fun NavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    startDestination: Any = BaseRoute
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier,
+    ) {
+        homeScreen(
+            onRecipeClick = navController::navigateToDetailRecipeScreen,
+            onToggleFavorite = {},
+            onOpenSection = navController::navigateToSection,
+            sectionDestination = {
+                sectionScreen(
+                    onBackClick = navController::popBackStack,
+                    onRecipeClick = navController::navigateToDetailRecipeScreen,
+                    onToggleFavorite = {}) {
+                    detailRecipeScreen(onBackClick = navController::popBackStack)
+                }
+            },
+            recipeDetailDestination = {
+                detailRecipeScreen(onBackClick = navController::popBackStack) }
+        )
+        categoriesScreen(
+            onOpenCategory = navController::navigateToCategoryScreen,
+            categoryDestination = {
+                categoryScreen(
+                    onOpenRecipe = navController::navigateToDetailRecipeScreen,
+                    onToggleFavorite = {},
+                    recipeDetailDestination = { detailRecipeScreen(onBackClick = navController::popBackStack) })
+            }
+        )
+        favoritesScreen(onToggleFavorite = {}, onOpenRecipe = navController::navigateToDetailRecipeScreen, recipeDetailDestination = {detailRecipeScreen(onBackClick = navController::popBackStack)})
+    }
+}
+
+
