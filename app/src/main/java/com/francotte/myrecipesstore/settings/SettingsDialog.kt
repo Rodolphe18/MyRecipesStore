ab04ee1a325cf0f8dac8520520738d3f86cbf380
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-06-04 14:45:58 +0200

    SettingsDialog created

---
+++ app/src/main/java/com/francotte/myrecipesstore/settings/SettingsDialog.kt
@@ -0,0 +1,116 @@
+package com.francotte.myrecipesstore.settings
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import com.francotte.myrecipesstore.R
+
+
+@Composable
+fun SettingsDialog(
+    onDismiss: () -> Unit
+) {
+    val configuration = LocalConfiguration.current
+    AlertDialog(
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+        modifier = Modifier.widthIn(max = configuration.screenWidthDp.dp - 80.dp),
+        containerColor = Color.White,
+        onDismissRequest = { onDismiss() },
+        title = {
+            Text(
+                text = stringResource(R.string.feature_settings_title),
+                style = MaterialTheme.typography.titleLarge,
+                color = Color.Black,
+                fontWeight = FontWeight.SemiBold
+            )
+        },
+        text = {
+            HorizontalDivider(color = Color.Black)
+            Column(Modifier.verticalScroll(rememberScrollState())) {
+                LinksPanel()
+            }
+        },
+        confirmButton = {
+            Text(
+                text = stringResource(R.string.feature_settings_dismiss_dialog_button_text),
+                style = MaterialTheme.typography.labelLarge,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .clickable { onDismiss() },
+            )
+        },
+    )
+}
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun LinksPanel() {
+    FlowRow(
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 16.dp,
+            alignment = Alignment.CenterHorizontally,
+        ),
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        WeatherAppTextButton(
+            onClick = { },
+        ) {
+            Text(text = stringResource(R.string.feature_settings_privacy_policy))
+        }
+        WeatherAppTextButton(
+            onClick = {},
+        ) {
+            Text(text = stringResource(R.string.feature_settings_licenses))
+        }
+        WeatherAppTextButton(
+            onClick = { },
+        ) {
+            Text(text = stringResource(R.string.feature_settings_brand_guidelines))
+        }
+        WeatherAppTextButton(
+            onClick = { },
+        ) {
+            Text(text = stringResource(R.string.feature_settings_feedback))
+        }
+    }
+}
+
+@Composable
+fun WeatherAppTextButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    content: @Composable RowScope.() -> Unit,
+) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.textButtonColors(contentColor = Color.Black, containerColor = Color.White),
+        content = content,
+    )
+}
