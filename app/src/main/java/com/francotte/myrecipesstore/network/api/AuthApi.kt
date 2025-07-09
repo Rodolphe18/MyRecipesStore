4b834f319cb98dd00da8989ea2bc68eb79840fa8
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-07-09 13:02:50 +0200

    Wording

--- app/src/main/java/com/francotte/myrecipesstore/network/api/AuthApi.kt
+++ app/src/main/java/com/francotte/myrecipesstore/network/api/AuthApi.kt
@@ -2,6 +2,8 @@ package com.francotte.myrecipesstore.network.api
 
 import com.francotte.myrecipesstore.network.model.AuthRequest
 import com.francotte.myrecipesstore.network.model.AuthResponse
+import com.francotte.myrecipesstore.network.model.FacebookAccessTokenRequest
+import com.francotte.myrecipesstore.network.model.GoogleIdTokenRequest
 import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.Body
@@ -13,15 +15,29 @@ import retrofit2.http.Path
 
 interface AuthApi {
 
+    // Manage User by Email/Password
+
     @POST("users/create")
     suspend fun createUser(
         @Body request: AuthRequest
     ): AuthResponse
 
     @POST("users/auth")
-    suspend fun authenticate(
+    suspend fun authUser(
         @Body request: AuthRequest
     ): Response<AuthResponse>
+
+    @POST("users/create/google")
+    suspend fun createGoogle(@Body request: GoogleIdTokenRequest): Response<AuthResponse>
+
+    @POST("users/auth/google")
+    suspend fun authGoogle(@Body request: GoogleIdTokenRequest): Response<AuthResponse>
+
+    @POST("users/create/facebook")
+    suspend fun createFacebook(@Body request: FacebookAccessTokenRequest): Response<AuthResponse>
+
+    @POST("users/auth/facebook")
+    suspend fun authFacebook(@Body request: FacebookAccessTokenRequest): Response<AuthResponse>
 
     @DELETE("users/{userId}")
     suspend fun deleteUser(@Path("userId") userId: Long)
