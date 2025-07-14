e1420f478c0cbf57056ee54ff0ff8dd5977b756f
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-07-14 15:50:20 +0200

    AuthRequest.kt updated

--- app/src/main/java/com/francotte/myrecipesstore/network/model/AuthRequest.kt
+++ app/src/main/java/com/francotte/myrecipesstore/network/model/AuthRequest.kt
@@ -4,10 +4,13 @@ import kotlinx.serialization.Serializable
 
 
 @Serializable
-data class AuthRequest(val username:String, val password:String)
+data class AuthRequest(val userNameOrMail:String, val password:String)
 
 @Serializable
 data class GoogleIdTokenRequest(val id_token: String)
 
 @Serializable
 data class FacebookAccessTokenRequest(val access_token: String)
+
+@Serializable
+data class EmailRequest(val email: String)
