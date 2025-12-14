46c3a72308a57ada15081916adf07f384dc0d330
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-12-14 16:42:44 +0100

    HomeViewModel.kt updated with a MutableSharedFlow to refresh VM

--- feature/home/src/main/java/com/francotte/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/francotte/home/HomeViewModel.kt
@@ -6,47 +6,82 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.francotte.common.restartableWhileSubscribed
 import com.francotte.data.repository.LikeableLightRecipesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(repository: LikeableLightRecipesRepository) : ViewModel() {
+@OptIn(ExperimentalCoroutinesApi::class)
+class HomeViewModel @Inject constructor(
+    private val repository: LikeableLightRecipesRepository
+) : ViewModel() {
 
     var currentPage by mutableIntStateOf(0)
+        private set
 
     var isReloading by mutableStateOf(false)
+        private set
 
-    val latestRecipes = repository.observeLatestRecipes()
+    private val refreshTrigger = MutableSharedFlow<Unit>(replay = 1)
+
+
+    val latestRecipes = refreshTrigger
+        .flatMapLatest { repository.observeLatestRecipes() }
         .mapToLatestRecipes()
-        .stateIn(viewModelScope, restartableWhileSubscribed, LatestRecipes.Loading)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = LatestRecipes.Loading
+        )
 
-    val americanRecipes = repository.observeAmericanAreaRecipes()
+    val americanRecipes = refreshTrigger
+        .flatMapLatest { repository.observeAmericanAreaRecipes() }
         .mapToAmericanRecipes()
-        .stateIn(viewModelScope, restartableWhileSubscribed, AmericanRecipes.Loading)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = AmericanRecipes.Loading
+        )
 
-    val englishRecipes = repository.observeEnglishAreaRecipes()
+    val englishRecipes = refreshTrigger
+        .flatMapLatest { repository.observeEnglishAreaRecipes() }
         .mapToEnglishRecipes()
-        .stateIn(viewModelScope, restartableWhileSubscribed, EnglishRecipes.Loading)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = EnglishRecipes.Loading)
 
-    val areasRecipes = repository.observeFoodAreaSections()
+
+    val areasRecipes = refreshTrigger
+        .flatMapLatest { repository.observeFoodAreaSections() }
         .mapToAreasRecipes()
-        .stateIn(viewModelScope, restartableWhileSubscribed, AreasRecipes.Loading)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = AreasRecipes.Loading
+        )
 
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        viewModelScope.launch {
+            refreshTrigger.emit(Unit)
+        }
+    }
 
     fun reload() {
         viewModelScope.launch {
             isReloading = true
-            delay(1000)
-            restartableWhileSubscribed.restart()
+            refreshTrigger.emit(Unit)
             isReloading = false
         }
     }
-
-
 }
-
