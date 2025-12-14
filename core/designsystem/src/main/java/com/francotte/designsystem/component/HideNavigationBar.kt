d9e8fb42893ad41e0741297725f67bfac4fde95d
rfrancotte <rodolphe.francotte@webedia-group.com>
2025-12-14 11:36:32 +0100

    HideNavigationBar composable created to hide status bar

---
+++ core/designsystem/src/main/java/com/francotte/designsystem/component/HideNavigationBar.kt
@@ -0,0 +1,33 @@
+package com.francotte.designsystem.component
+
+import android.view.Window
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
+
+@Composable
+fun HideNavigationBar(window: Window) {
+    val view = LocalView.current
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+
+    DisposableEffect(window, view, lifecycle) {
+        val controller = WindowCompat.getInsetsController(window, view)
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME) controller.hide(WindowInsetsCompat.Type.systemBars())
+        }
+        lifecycle.addObserver(observer)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        onDispose {
+            lifecycle.removeObserver(observer)
+            controller.show(WindowInsetsCompat.Type.systemBars())
+        }
+    }
+}
--- feature/video/src/main/java/com/francotte/video/HideNavigationBar.kt
+++ feature/video/src/main/java/com/francotte/video/HideNavigationBar.kt
@@ -11,23 +11,3 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 
-@Composable
-fun HideNavigationBar(window: Window) {
-    val view = LocalView.current
-    val lifecycle = LocalLifecycleOwner.current.lifecycle
-
-    DisposableEffect(window, view, lifecycle) {
-        val controller = WindowCompat.getInsetsController(window, view)
-        controller.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        val observer = LifecycleEventObserver { _, event ->
-            if (event == Lifecycle.Event.ON_RESUME) controller.hide(WindowInsetsCompat.Type.systemBars())
-        }
-        lifecycle.addObserver(observer)
-        controller.hide(WindowInsetsCompat.Type.systemBars())
-        onDispose {
-            lifecycle.removeObserver(observer)
-            controller.show(WindowInsetsCompat.Type.systemBars())
-        }
-    }
-}
