7cbd7cdad545b72901a11285d62900756f536892
rfrancotte <rodolphe.francotte@webedia-group.com>
2026-01-10 11:39:41 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.francotte.myrecipesstore"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.2.5"
+        versionCode = 22
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
