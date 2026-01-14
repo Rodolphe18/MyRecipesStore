6b26ae96f62066b1ca14ad2de6edf190bc3e5a8d
rfrancotte <rodolphe.francotte@webedia-group.com>
2026-01-14 14:46:34 +0100

    WorkManager created via AppInitializer & EntrePoint Accessor (no more Hilt)

--- app/src/main/java/com/francotte/myrecipesstore/FoodApplication.kt
+++ app/src/main/java/com/francotte/myrecipesstore/FoodApplication.kt
@@ -1,42 +1,24 @@
 package com.francotte.myrecipesstore
 
 import android.app.Application
-import android.content.Context
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.hilt.work.HiltWorkerFactory
 import androidx.lifecycle.ProcessLifecycleOwner
-import androidx.work.Configuration
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.francotte.billing.BillingAppLifecycleObserver
 import com.francotte.common.counters.LaunchCounter
-import com.francotte.notifications.DailyNotificationWorkManager
 import com.google.android.gms.ads.MobileAds
 import com.google.firebase.Firebase
 import com.google.firebase.messaging.messaging
 import dagger.hilt.android.HiltAndroidApp
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltAndroidApp
-class FoodApplication:Application(), Configuration.Provider {
-
-    @Inject
-    override lateinit var workManagerConfiguration: Configuration
+class FoodApplication:Application() {
 
     @Inject
     lateinit var billingObserver: BillingAppLifecycleObserver
 
     @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    @Inject
     lateinit var launchCounter: LaunchCounter
-
 
     override fun onCreate() {
         super.onCreate()
@@ -47,5 +29,7 @@ class FoodApplication:Application(), Configuration.Provider {
             .addOnFailureListener { e -> Log.e("FCM", "Subscribe failed", e) }
         ProcessLifecycleOwner.get().lifecycle.addObserver(billingObserver)
     }
+
+
 
 }
