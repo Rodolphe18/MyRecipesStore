bd38050eedd59e28e99c331b81bd234d234fdb2b
rodolphe <rodolphefrancotte18@gmail.com>
2026-09-08 11:56:36 +0200

    upgrade version code to 25 & version name to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.francotte.myrecipesstore"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.2.9"
+        versionCode = 25
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
