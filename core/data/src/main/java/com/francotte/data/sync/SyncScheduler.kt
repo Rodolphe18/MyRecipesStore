22ffafbb13c9b452730a7f060f167d93ba9845a8
rodolphe <rodolphefrancotte18@gmail.com>
2026-09-08 11:43:01 +0200

    former files from module 'sync' deleted

--- core/data/src/main/java/com/francotte/data/sync/SyncScheduler.kt
+++
@@ -1,8 +0,0 @@
-package com.francotte.data.sync
-
-import android.content.Context
-
-interface SyncScheduler {
-    fun enqueueForLogin(context: Context)
-    fun enqueueForToggle(context: Context)
-}
